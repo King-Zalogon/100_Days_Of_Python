298ef8b246d584252b7f3b56dc37ea40f0af0fe3
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-09-26 14:06:04 -0300

    EoD 021

--- Days 020 021 - Snake Game/main.py
+++ Days 020 021 - Snake Game/main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("My Python Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,13 +24,20 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.15)
     snake.move()
+    if snake.head.distance(food) < 15:
+        scoreboard.scoreup()
+        food.refresh()
+        snake.extend()
 
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 280 or snake.head.ycor() < -290:
+        scoreboard.game_over()
+        game_is_on = False
 
+    for segment in snake.snake_body[2::]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
-
-
-
-# Note to self: Code before this
 screen.exitonclick()
