fef84c2aea8faf40c03a0191b4117765576f2233
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-09-30 18:00:31 -0300

    EoD 024

--- Day 024/Mail Merge Project Start/main.py
+++ Day 024/Mail Merge Project Start/main.py
@@ -1,11 +1,11 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 names = []
 template = ''
@@ -14,7 +14,7 @@ with open('./Input/Names/invited_names.txt') as file:
     names = file.readlines()
 
 for i in range(len(names)):
-    names[i] = names[i][0: -1]
+    names[i] = names[i].strip()
 
 with open('./Input/Letters/starting_letter.txt') as file:
     template = file.read()
@@ -22,4 +22,3 @@ with open('./Input/Letters/starting_letter.txt') as file:
 for name in names:
     with open(f'./Output/ReadyToSend/{name}.txt', mode='w') as file:
         file.write(f"{template.replace('[name]', name)}")
-
