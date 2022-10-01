b8ab05e85716d14ddc1ee501f96d0e2f18c4055b
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-01 09:19:55 -0300

    SoD 025

--- Day 025/main.py
+++ Day 025/main.py
@@ -17,5 +17,34 @@
 
 import pandas
 
-data = pandas.read_csv('weather_data.csv')
-print(data['temp'])
+# data = pandas.read_csv('weather_data.csv')
+# print(data['temp'])
+# print(type(data))
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+#
+# avg_temp = sum(temp_list)/len(temp_list)
+# print(avg_temp)
+#
+# print(data['temp'].mean())
+#
+# print(data['temp'].max())
+# print(data['condition'])
+# print(data.condition)
+#
+# print(data[data.temp == data.temp.max()])
+#
+# monday = (data[data.day == 'Monday']).temp * 9/5 + 32
+# print(monday)
+
+data_dict = {
+    'students': ['Any', 'James', 'Angela'],
+    'scores': [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv('new_data.csv')
