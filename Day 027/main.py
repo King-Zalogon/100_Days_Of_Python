deb66847489c79209c6ed4740df25cf5512866f8
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-04 14:33:44 -0300

    MiD 027

--- Day 027/main.py
+++ Day 027/main.py
@@ -8,4 +8,14 @@ my_label = tkinter.Label(text= "I'm a Mechanicus label", font=('Arial', 18, 'bol
 my_label.pack()     # A geometry management system
 
 
+def add(*jungle):
+    n = 0
+    for bananas in jungle:
+        n += bananas
+    print(n)
+    print(jungle[-1])
+
+
+add(1, 3, 5, 8, 4, 9)
+
 window.mainloop()
