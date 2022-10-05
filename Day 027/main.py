baea41ad7cb56e731b8b4e50229e98a87f71f486
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-05 01:43:05 -0300

    MiN commit

--- Day 027/main.py
+++ Day 027/main.py
@@ -16,6 +16,34 @@ def add(*jungle):
     print(jungle[-1])
 
 
-add(1, 3, 5, 8, 4, 9)
+# add(1, 3, 5, 8, 4, 9)
+
+
+def calculate(n, **kargs):
+    print(kargs)
+    n += kargs['add']
+    n *= kargs['multiply']
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get('make')
+        self.model = kw.get('model')
+        self.color = kw.get('color')
+        self.seats = kw.get('seats')
+
+
+my_car = Car(make='Nissan')
+print(my_car.make)
+print(my_car.model)
+
+def test(*args):
+    print(args)
+
+test(1,2,3,4)
 
 window.mainloop()
