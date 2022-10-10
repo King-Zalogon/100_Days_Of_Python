7d0b094079cd3d9d56dcb8293fbeb6897245f268
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-10 20:21:02 -0300

    MiD commit

--- Day 026/NATO-alphabet-start/main.py
+++ Day 026/NATO-alphabet-start/main.py
@@ -2,17 +2,20 @@ import pandas
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-#TODO 1. Create a dictionary in this format:
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 
 def word_to_nato(word_dict):
     word = input('Enter a word: ')
-    letter_list = [letter.upper() for letter in word if letter.isalpha()]
-    word_list = [word_dict[letter] for letter in letter_list]
-    print(word_list)
+    try:
+        # letter_list = [letter.upper() for letter in word if letter.isalpha()]
+        # word_list = [word_dict[letter] for letter in letter_list]
+        word_list = [word_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        word_to_nato(word_dict)
+    else:
+        print(word_list)
 
 
 word_to_nato(nato_dict)
--- Day 029/password-manager-start/main.py
+++ Day 029/password-manager-start/main.py
@@ -1,14 +1,18 @@
+
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 def pass_gen():
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -32,21 +36,67 @@ def pass_save():
     site = site_entry.get()
     user = user_entry.get()
     password = pass_entry.get()
+    new_data = {
+        site: {
+            "email": user,
+            "password": password,
+        }
+    }
 
     if len(site) == 0 or len(user) == 0 or len(password) == 0:
-        messagebox.showwarning(title='Ooops!', message="Please don't leave any field empty")
+        messagebox.showwarning(title='Oops!', message="Please don't leave any field empty")
 
     else:
-        is_ok = messagebox.askokcancel(title=site, message=f"These are the details entered:"
-                                               f"\nUser/Email: {user}"
-                                               f"\nPassword: {password}")
+        is_ok = messagebox.askokcancel(title=site, message=f"These are the details entered:\nUser or Email: {user}"
+                                                           f"\nPassword: {password}")
 
         if is_ok:
-            with open('data.txt', mode='a') as file:
-                file.write(f"{site} | {user} | {password}\n")
-            site_entry.delete(0, END)
-            user_entry.delete(0, END)
-            pass_entry.delete(0, END)
+            try:
+                with open('data.json', mode='r') as file:
+                    data = json.load(file)  # Reading old data
+
+            except FileNotFoundError:
+                with open('data.json', mode='w') as file:
+                    json.dump(new_data, file, indent=4)
+
+            else:
+                data.update(new_data)  # Updating old data with new data
+
+                with open('data.json', mode='w') as file:
+                    json.dump(data, file, indent=4)  # Saving updated data
+                    # file.write(f"{site} | {user} | {password}\n") <-- Old code
+            finally:
+                site_entry.delete(0, END)
+                pass_entry.delete(0, END)
+
+# ---------------------------- SEARCH BUTTON ------------------------------- #
+
+
+def pass_search():
+    web = site_entry.get()
+
+    try:
+        with open('data.json', mode='r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning(f"No {file} database found")
+    else:
+        try:
+            election = messagebox.askquestion(
+                title="Search results",
+                message=f"{web}\nEmail: {data[web]['email']}\nPassword: {data[web]['password']}\nDo you want to copy "
+                        f"your password to the clipboard?"
+            )
+
+        except KeyError:
+            messagebox.showwarning(title="Info not found", message=f"No stored details for {web}")
+
+        else:
+            if election == 'yes':
+                pyperclip.copy(data[web]['password'])
+            else:
+                pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -67,22 +117,22 @@ pass_label = Label(text='Password:')
 pass_label.grid(column=0, row=3)
 pass_label.config(padx=5, pady=5)
 
-site_entry = Entry(width=35)
-site_entry.grid(column=1, row=1, columnspan=2)
+site_entry = Entry(width=25)
+site_entry.grid(column=1, row=1)
 site_entry.focus()
 
-
-user_entry = Entry(width=35)
+user_entry = Entry(width=43)
 user_entry.grid(column=1, row=2, columnspan=2)
 user_entry.insert(0, "some_mail_address@gmail.com")
 
-
-pass_entry = Entry(width=17)
+pass_entry = Entry(width=25)
 pass_entry.grid(column=1, row=3)
 
+search_button = Button(text='Search', width=14 , command=pass_search)
+search_button.grid(column=2, row=1, pady=5)
 
 generate_button = Button(text='Generate Password', command=pass_gen)
-generate_button.grid(column=2, row=3)
+generate_button.grid(column=2, row=3, pady=5)
 
 add_button = Button(text='Add', width=30, command=pass_save)
 add_button.grid(column=1, row=4, columnspan=2, pady=5)
