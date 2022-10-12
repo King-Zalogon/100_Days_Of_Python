70b73372812d80a8329e9fe9bf51df47be9a232a
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-11 23:58:32 -0300

    Update main.py

--- Day 031/flash-card-project-start/main.py
+++ Day 031/flash-card-project-start/main.py
@@ -1,4 +1,20 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
-my_image = PhotoImage(file="path/to/image_file.png")
-button = Button(image=my_image, highlightthickness=0)
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="card_front.png")
+
+right_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_img, highlightthickness=0)
+
+wrong_img = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+
+
+# ------------------------------ Code Above This ------------------------------ #
+window.mainloop()
