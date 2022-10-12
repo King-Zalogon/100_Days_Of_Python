163acc1cb5266c8b5f16cf0de9abe446aa273a3a
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-12 17:28:16 -0300

    EoD 031

--- Day 031/flash-card-project-start/main.py
+++ Day 031/flash-card-project-start/main.py
@@ -3,39 +3,60 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+BLACK = "#000000"
+WHITE = "#ffffff"
+
+
+def next_card():
+    global current_card, timer
+    window.after_cancel(timer)
+    current_card = random.choice(to_learn)
+    italian_card()
+    timer = window.after(3000, func=english_card)
+    to_learn.remove(current_card)
+    new_data = data.from_dict(to_learn, orient="columns")
+    new_data.to_csv('data/words_to_learn.csv', index=False)
+
+
+def italian_card():
+    canvas.itemconfig(background, image=card_front_img)
+    canvas.itemconfig(card_title, text='Italian', fill=BLACK)
+    canvas.itemconfig(card_word, text=current_card['Italian'], fill=BLACK)
+
+
+def english_card():
+    canvas.itemconfig(background, image=card_back_img)
+    canvas.itemconfig(card_title, text='English', fill=WHITE)
+    canvas.itemconfig(card_word, text=current_card['English'], fill=WHITE)
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-data = pandas.read_csv("data/italian_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    base_data = pandas.read_csv("data/italian_words.csv")
+    base_data.to_csv("data/words_to_learn.csv", index=False)
+    data = pandas.read_csv("data/words_to_learn.csv")
+finally:
+    to_learn = data.to_dict(orient="records")
 
 
-def next_card():
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text='Italian')
-    canvas.itemconfig(card_word, text=current_card['Italian'])
+current_card = {}
 
-def get_word():
-    global word
-    global title
-    canvas.delete(word)
-    word = canvas.create_text(400, 250, text=random_word(word_dict)[0], font=('Arial', 60, "bold"))
-    canvas.delete(title)
-    title = canvas.create_text(400, 125, text="Italian", font=("Ariel", 40, "italic"))
-
+timer = window.after(3000, func=english_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=card_front_img)
+background = canvas.create_image(400, 263, image=card_front_img)
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 125, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 250, text="word", font=('Arial', 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
-
 
 right_img = PhotoImage(file="images/right.png")
 right_button = Button(image=right_img, highlightthickness=0, command=next_card)
@@ -45,6 +66,6 @@ wrong_img = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
-next_card()
+
 # ------------------------------ Code Above This ------------------------------ #
 window.mainloop()
