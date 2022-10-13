ec24a38d2893f1e1fdb3fc3318a1600b212673cf
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-13 17:14:20 -0300

    MiD commit

--- Day 032/Birthday Wisher (Day 32) start/main.py
+++ Day 032/Birthday Wisher (Day 32) start/main.py
@@ -1,16 +1,64 @@
-# import smtplib
-#
-# my_email = "zalogonking@gmail.com"
-# password = 'notapassword'
-#
-# gmail_smtp = 'smtp.gmail.com'
-# hotmail_smtp = 'smtp.live.com'
-# yahoo_smtp = 'smtp.mail.yahoo.com'
-#
-# with smtplib.SMTP('smtp.gmail.com', port=587) as connection:  # to connect to de smtp of our mail
-#     connection.starttls()  # to encrypt the messages
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="zalogonking@yahoo.com",
-#                         msg='Subject: Praise the Omnissiah\n\nThis is the body of my test email using python.')
-#     # connection.close()  # always close afterwards if i don't use the "with" method
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = data.to_dict(orient='records')
+print(birthdays_dict)
+
+now = dt.datetime.now()
+month = now.month
+day = now.day
+today_name = ''
+today_email = ''
+
+# 2. Check if today matches a birthday in the birthdays.csv
+birthday = False
+
+for i in range(len(birthdays_dict)):
+    if birthdays_dict[i]['month'] == month and birthdays_dict[i]['day'] == day:
+        today_email = birthdays_dict[i]['email']
+        today_name = birthdays_dict[i]['name']
+        birthday = True
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name
+# from birthdays.csv
+
+letters = []
+for i in range(1,4):
+    with open(f'letter_templates/letter_{i}.txt') as file:
+        letters.append(file.read())
+
+print(letters)
+
+template = random.choice(letters)
+today_message = template.replace('[NAME]', f'{today_name}')
+
+
+with open('quotes.txt') as text:
+    quotes_list = text.readlines()
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+my_email = "zalogonking@gmail.com"
+password = 'notapassword'
+
+gmail_smtp = 'smtp.gmail.com'
+hotmail_smtp = 'smtp.live.com'
+yahoo_smtp = 'smtp.mail.yahoo.com'
+
+
+if birthday:
+    today_quote = random.choice(quotes_list)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:  # to connect to de smtp of our mail
+        connection.starttls()  # to encrypt the messages
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=today_email,
+                            msg=f'Subject: Happy B-day {today_name}!!!\n\n{today_message}\n{today_quote}'
+                            )
+
--- Day 032/main.py
+++ Day 032/main.py
@@ -1,2 +1,36 @@
 import smtplib
+import datetime as dt
+import random
 
+now = dt.datetime.now()
+year = now.year
+month = now.month  # starts at 1 for January
+day_of_the_week = now.weekday()  # starts at 0 for monday
+print(day_of_the_week)
+
+birth_date = dt.datetime(year=1990, month=12, day=12, hour=4, minute=30)
+print(birth_date)
+
+my_email = "zalogonking@gmail.com"
+password = 'notapassword'
+
+gmail_smtp = 'smtp.gmail.com'
+hotmail_smtp = 'smtp.live.com'
+yahoo_smtp = 'smtp.mail.yahoo.com'
+
+with open('quotes.txt') as text:
+    quotes_list = text.readlines()
+
+now = dt.datetime.now()
+day_of_the_week = now.weekday()
+if day_of_the_week == 3:
+    today_quote = random.choice(quotes_list)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:  # to connect to de smtp of our mail
+        connection.starttls()  # to encrypt the messages
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="zalogonking@gmail.com",
+                            msg=f'Subject: Here is your quote!\n\n{today_quote}'
+                            )
+    print(day_of_the_week)
+    print(today_quote)
