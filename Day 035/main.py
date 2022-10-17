6fa86b6aece29a7fdac39880e095342a069132d9
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-17 18:20:26 -0300

    MiD Commit

--- Day 035/main.py
+++ Day 035/main.py
@@ -1,4 +1,19 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MY_LAT = -34.603683
+MY_LNG = -58.381557
+MY_POSS = (MY_LNG, MY_LAT)
+
+parameters = {
+    'appid': "22b4fd0b83c519a0dbd9f95b66103ee8",
+    'lat': MY_LAT,
+    'lon': MY_LNG,
+    'units': "metric"
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameters)
+
+data = response.json()
+
+print(data)
+
