b5dc5a4e36b818553474d92fc6d00eea934ddc62
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-18 19:02:46 -0300

    MiD commit

--- Day 036/main.py
+++ Day 036/main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+# Stock Prices Monitor App
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+STOCK_KEY = "L2MAIFZDE50V79MU"
+
+# TODO 1) Pull the Stock Price for Day N, the the price for N-1
+# How mucho it increased or decreased, in amount and in percentage
+
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "IBM",
+    "apikey": STOCK_KEY
+}
+
+stock_daily_url = 'https://www.alphavantage.co/query'
+r = requests.get(stock_daily_url, parameters)
+data = r.json()
+
+print(data)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# TODO 2) Relevant New for that Stock's Company to understand the reason
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# TODO 3) Send us SMS from Twilio with the facts to decide.
