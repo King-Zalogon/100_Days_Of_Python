630602f53531a69d5cd421dffce78d7e77e59baf
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2022-10-24 22:50:08 -0300

    EoD commit

--- Day 039 - 040 - Flight Deals/flight_search.py
+++ Day 039 - 040 - Flight Deals/flight_search.py
@@ -1,8 +1,20 @@
+import requests
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self, **kwargs):
         self.iata_code = kwargs.get('iataCode')
+        self.city = kwargs.get('city')
+        self.apikey = kwargs.get('apikey')
+        self.endpoint = "https://api.tequila.kiwi.com/locations/query"
+        self.header = {'apikey': self.apikey}
+        self.parameters = {"term": self.city, "locations_types": "city"}
+        self.response = None
         self.price = "TESTING"
 
-    def code_search(self, **kwargs):
-        pass
+    def code_search(self):
+        self.response = requests.get(self.endpoint, headers=self.header, params=self.parameters)
+        self.response.raise_for_status()
+        result = self.response.json()
+        return result['locations'][0]['code']
--- Day 039 - 040 - Flight Deals/main.py
+++ Day 039 - 040 - Flight Deals/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from data_manager import DataManager
-from env import username, project, sheet, token
+from env import username, project, sheet, token, apikey
 from flight_search import FlightSearch
 
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
@@ -12,10 +12,13 @@ sheet_data = DataManager(username=username, project=project, sheet=sheet, token=
 
 for i in range(0, len(sheet_data.prices)):
     code = sheet_data.prices[i]['iata']
-    flight = FlightSearch(iataCode=code)
+    city = sheet_data.prices[i]['city']
+    print(city)
+    flight = FlightSearch(iataCode=code, city=city, apikey=apikey)
+    print(flight.code_search())
     parameters = {"price": {
-        "lowest": flight.price
+        "iata": flight.code_search()
     }}
     sheet_data.put_data(object_id=sheet_data.prices[i]['id'], parameters=parameters)
 
-print(sheet_data.prices)
+# print(sheet_data.prices)
