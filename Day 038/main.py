276d3c7cad79beced51d040883ac7feac9f3b685
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-08-04 16:46:40 -0300

    Update main.py

--- Day 038/main.py
+++ Day 038/main.py
@@ -1,26 +1,29 @@
 import requests
 from datetime import datetime
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-GENDER = "male"
-WEIGHT_KG = "70"
-HEIGHT_CM = "177"
-AGE = "33"
+# CONSTANTS AND ENVIRO VARS
+GENDER = os.getenv('GEN')
+WEIGHT_KG = os.getenv('WEIGHT')
+HEIGHT_CM = os.getenv('HEIGHT')
+AGE = os.getenv('AGE')
 
-os.environ["APP_ID"] = ""
-os.environ["API_KEY"] = ""
-print(os.environ)
-APP_ID = os.environ["APP_ID"]
-API_KEY = os.environ["API_KEY"]
+# os.environ["APP_ID"] = ""
+# os.environ["API_KEY"] = ""
+# print(os.environ)
+APP_ID = os.getenv('NUTRITIONIX_ID')
+API_KEY = os.getenv('NUTRITIONIX_KEY')
 
 NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-SHEETY_USERNAME = ""
-SHEETY_PROJECT_NAME = "myWorkouts"
-SHEETY_SHEET_NAME = "workouts"
+SHEETY_USERNAME = os.getenv('SHEETY_USER')
+SHEETY_PROJECT_NAME = "exerciseTracker"
+SHEETY_SHEET_NAME = "exercises"
 SHEETY_ENDPOINT = f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT_NAME}/{SHEETY_SHEET_NAME}"
-SHEETY_TOKEN = ""
+SHEETY_TOKEN = os.getenv('SHEETY_TKN')
 
 date = datetime.now().strftime("%d/%m/%Y")
 time = datetime.now().strftime("%I:%M:%S %p")
@@ -32,8 +35,8 @@ exercise_text = input("Which exercises did you complete today? ")
 nutritionix_headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
-    # "x-remote-user-id": "",
-    # "x-user-jwt": "",
+    # "x-remote-user-id": APP_ID,
+    # "x-user-jwt": API_KEY,
 }
 
 nutritionix_parameters = {
@@ -44,7 +47,7 @@ nutritionix_parameters = {
     "age": AGE,
 }
 
-nutritionix_response = requests.post(NUTRITIONIX_ENDPOINT, json=nutritionix_parameters, headers=nutritionix_headers)
+nutritionix_response = requests.post(url=NUTRITIONIX_ENDPOINT, json=nutritionix_parameters, headers=nutritionix_headers)
 nutritionix_response.raise_for_status()
 nutritionix_result = nutritionix_response.json()
 print(nutritionix_result)
@@ -79,19 +82,23 @@ print(today_workout)
 # Sheety get and post:
 
 sheety_header = {
-    "Authorization": SHEETY_TOKEN
+    "Authorization": f'Bearer {SHEETY_TOKEN}'
 }
 
 for param in today_workout:
     sheety_parameters = param
-    sheety_post = requests.post(SHEETY_ENDPOINT, headers=sheety_header, json=sheety_parameters)
+    sheety_post = requests.post(
+        url='https://api.sheety.co/0878ab3260b3e8ff3ff452484f0f874a/exerciseTracker/exercises',
+        json=sheety_parameters,
+        headers=sheety_header
+    )
     sheety_post.raise_for_status()
-    # sheety_result_post = sheety_post.json()
-    # print(sheety_result_post)
+    sheety_result_post = sheety_post.json()
+    print(sheety_result_post)
 
-# sheety_response = requests.get(SHEETY_ENDPOINT, headers=sheety_header)
-# sheety_result_get = sheety_response.json()
+sheety_response = requests.get(SHEETY_ENDPOINT, headers=sheety_header)
+sheety_result_get = sheety_response.json()
 
-# print(sheety_result_post)
+print(sheety_result_post)
 
 
