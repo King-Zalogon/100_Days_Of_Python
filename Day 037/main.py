7851477d9a09816fcf1478bf277ac53cf40884b8
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-08-04 14:48:23 -0300

    Update main.py

--- Day 037/main.py
+++ Day 037/main.py
@@ -1,23 +1,32 @@
+import os
 import requests
 from _datetime import datetime as dt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PIXELA_URL = "https://pixe.la/v1/users"
-USER_NAME = ""
-TOKEN = ""
+USER_NAME = os.getenv('USER')
+TOKEN = os.getenv('TOKEN')
 GRAPH_ID = "graph1"
 TODAY = dt.now().strftime("%Y%m%d")
 
 # TODO Create a user account - DONE
-# user_parameters = {
-#     "token": "Not a token",
-#     "username": "zalogon",
-#     "agreeTermsOfService": "yes",
-#     "notMinor": "yes",
-# }
+
+user_parameters = {
+    "token": TOKEN,
+    "username": USER_NAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
 
 # response = requests.post(url=PIXELA_URL, json=user_parameters)
 # print(response.text)
+
 # response was: {"message":"Success. Let's visit https://pixe.la/@zalogon , it is your profile page!","isSuccess":true}
+# {"message":"Success. Let's visit https://pixe.la/@zalogonkin , it is your profile page!","isSuccess":true}
+# {"message":"Success. Let's visit https://pixe.la/@zalogonking , it is your profile page!","isSuccess":true}
+# {"message":"Specified graphID not exist.","isSuccess":false}
 
 # TODO Create a graph definition
 
@@ -27,43 +36,44 @@ TODAY = dt.now().strftime("%Y%m%d")
 #     "agreeTermsOfService": "yes",
 #     "notMinor": "yes",
 # }
-#
-# graph_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs"
-#
+
+graph_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs"
+
 # graph_config = {
-#     "id": "graph1",
+#     "id": GRAPH_ID,
 #     "name": "PushUps",
-#     "unit": "Pushes",
+#     "unit": "Push",
 #     "type": "int",
 #     "color": "momiji",
-# }
-#
+#     }
+
 # headers = {
 #     "X-USER-TOKEN": TOKEN
-# }
-#
+#     }
+
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
+
 # TODO View the graph
 
-graph_view = "https://pixe.la/v1/users/zalogon/graphs/graph1"
+graph_view = f'https://pixe.la/v1/users/{USER_NAME}/graphs/{GRAPH_ID}'
 
 # TODO Posting value into the graph
 
-# pixel_data = {
-#     "name" : "PushUps",
-#     "date": TODAY,
-#     "quantity": "50",
-#
-# }
+pixel_data = {
+    "name" : "PushUps",
+    "date": TODAY,
+    "quantity": "50",
+
+    }
 
 headers = {
     "X-USER-TOKEN": TOKEN
-}
+    }
 
 pixel_creation_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}"
-# Could add /{YYYYMMDD} to the end point to update a pixel
+Could add /{YYYYMMDD} to the end point to update a pixel
 
 # response = requests.put(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 # print(response.text)
@@ -82,10 +92,10 @@ pixel_creation_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}"
 
 # TODO Delete a pixel
 
-delete_pixel_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20221020"
-delete_pixel_data = {
-
-}
-
-response = requests.delete(url=delete_pixel_endpoint, headers=headers)
-print(response.text)
+# delete_pixel_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20221020"
+# delete_pixel_data = {
+#
+# }
+#
+# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+# print(response.text)
