b36aef3cae24847d0ec19ab7bdd466954ceeaac4
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-08-04 15:19:41 -0300

    Reworking day 37 - Progress tracker

--- Day 037/main.py
+++ Day 037/main.py
@@ -47,9 +47,9 @@ graph_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs"
 #     "color": "momiji",
 #     }
 
-# headers = {
-#     "X-USER-TOKEN": TOKEN
-#     }
+headers = {
+    "X-USER-TOKEN": TOKEN
+    }
 
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
@@ -57,45 +57,53 @@ graph_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs"
 
 # TODO View the graph
 
-graph_view = f'https://pixe.la/v1/users/{USER_NAME}/graphs/{GRAPH_ID}'
+graph_view = f'https://pixe.la/v1/users/{USER_NAME}/graphs/{GRAPH_ID}.html'
 
 # TODO Posting value into the graph
 
-pixel_data = {
-    "name" : "PushUps",
-    "date": TODAY,
-    "quantity": "50",
+# pixel_data = {
+#     "date": TODAY,
+#     "quantity": "10",
+#     }
 
-    }
+# headers = {
+#     "X-USER-TOKEN": TOKEN
+#     }
 
-headers = {
-    "X-USER-TOKEN": TOKEN
-    }
+# pixel_creation_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}"
+# Could add /{YYYYMMDD} to the end point to update a pixel
 
-pixel_creation_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}"
-Could add /{YYYYMMDD} to the end point to update a pixel
-
-# response = requests.put(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 # print(response.text)
 
 # TODO Update a pixel
 
-# update_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20221021"
+# update_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20230802"
 # pixel_data = {
 #     "name" : "PushUps",
-#     "date": "20221020",
-#     "quantity": "500",
+#     "date": "20230803",
+#     "quantity": "50",
 # }
-#
-# response = requests.put(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+
+# response = requests.put(url=update_endpoint, json=pixel_data, headers=headers)
 # print(response.text)
 
 # TODO Delete a pixel
 
-# delete_pixel_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20221020"
-# delete_pixel_data = {
+delete_pixel_endpoint = f"{PIXELA_URL}/{USER_NAME}/graphs/{GRAPH_ID}/20230803"
+delete_pixel_data = {
+
+}
+
+response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+print(response.text)
+
+
+
+# pin_graph_enpoint = f"{PIXELA_URL}/{USER_NAME}"
+# put_data = {
+#     "pinnedGraphID": GRAPH_ID
+#     }
 #
-# }
-#
-# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+# response = requests.put(url=pin_graph_enpoint, json=put_data, headers=headers)
 # print(response.text)
