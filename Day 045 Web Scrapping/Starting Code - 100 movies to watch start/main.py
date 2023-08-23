3a0d295ec28db152b95595c395e0998a7597015f
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-08-23 13:27:12 -0300

    Webscrapping Top 100 movies

--- Day 045 Web Scrapping/Starting Code - 100 movies to watch start/main.py
+++ Day 045 Web Scrapping/Starting Code - 100 movies to watch start/main.py
@@ -1,8 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
 
+response = requests.get(url=URL)
 
+movies_webpage = response.text
+
+soup = BeautifulSoup(movies_webpage, 'html.parser')
+
+movies = soup.find_all('h3', class_="listicleItem_listicle-item__title__hW_Kn")
+
+movies_list = []
+
+for movie in movies:
+    movies_list.append(movie.text)
+
+movies_list.reverse()
+
+with open('movies.txt', 'w', encoding='UTF-8') as file:
+    for movie in movies_list:
+        file.write(f'{movie}\n')
+
+with open('movies.txt', 'r', encoding='UTF-8') as file:
+    print(file.read())
