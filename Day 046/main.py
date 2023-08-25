1801b90b927b67ea247b5729c8f361aeaea5ad80
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-08-25 16:21:08 -0300

    Update main.py

--- Day 046/main.py
+++ Day 046/main.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import spotipy
-from spotipy import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
@@ -10,10 +10,12 @@ load_dotenv()
 CLIENT_ID = os.getenv('ID')
 CLIENT_SECRET = os.getenv('SECRET')
 
+
 # ranking_date = input('Enter the date in a "YYYY-MM-DD" format:')
 ranking_date = '1982-12-14'
 
 URL = 'https://www.billboard.com/charts/hot-100/'
+REDIRECT_URI = os.getenv('REDIRECT')
 
 response = requests.get(url=f'{URL}{ranking_date}/')
 
@@ -31,3 +33,14 @@ song_authors_spans = soup.select(selector='li ul li span',
 song_authors = [author.getText().strip() for author in song_authors_spans]
 song_authors = [author for author in song_authors if len(author) > 2]
 
+# Now to use the Spotify API
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope="playlist-modify-private"))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
