4e92033a6d6ef36f2ee5bbf35830b1a20fae17fb
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-09-10 19:32:30 -0300

    Update main.py

--- Day 048 - Selenium Webdriver/main.py
+++ Day 048 - Selenium Webdriver/main.py
@@ -18,18 +18,36 @@ driver = webdriver.Chrome(chrome_options)
 
 # Python.org search bar
 driver.get(python_url)
-search_bar = driver.find_element(By.NAME, value="q")
-print(search_bar.get_attribute("placeholder"))
 
-button = driver.find_element(By.ID, value="submit")
-print(button.size)
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+#
+# button = driver.find_element(By.ID, value="submit")
+# print(button.size)
+#
+# documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(documentation_link.text)
+#
+# # XPath (XML Path Language)
+# bug_link = driver.find_element(By.XPATH, value='/html/body/div/footer/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
 
-documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
-print(documentation_link.text)
+# events = driver.find_elements(By.CLASS_NAME, value='event-widget li')
+# events_dict = {}
+# key_number = 0
+# for event in events:
+#     date_and_name = event.text.split("\n")
+#     date = date_and_name[0]
+#     name = date_and_name[1]
+#     events_dict[key_number]={'time':date, 'name':name}
+#     key_number += 1
 
-# XPath (XML Path Language)
-bug_link = driver.find_element(By.XPATH, value='/html/body/div/footer/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
+event_times = driver.find_elements(By.CSS_SELECTOR, value='.event-widget time')
+event_names = driver.find_elements(By.CSS_SELECTOR, value='.event-widget li a')
+events = {n: {'time': event_times[n].text, 'name': event_names[n].text} for n in range(0, len(event_times))}
+
+print(events)
+
 # To close a single active tab
 # driver.close()
 
